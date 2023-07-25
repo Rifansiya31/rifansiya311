56e6cfc80ea9bdf70e245dd27c0f449959009137
rifansiya31 <rifansiya31@gmail.com>
2023-07-25 20:35:53 +0530

    1st update

---
+++ src/test/java/demo1/demo.java
@@ -0,0 +1,17 @@
+package demo1;
+
+import org.junit.Test;
+
+public class demo {
+      @Test
+		private void test4() {
+			System.out.println("test-4");
+	
+	}
+    @Test
+	private void test5() {
+       System.out.println("test-5");
+	}
+	}
+
+
